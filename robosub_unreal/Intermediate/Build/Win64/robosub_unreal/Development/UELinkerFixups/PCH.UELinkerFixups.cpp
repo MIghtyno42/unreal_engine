e89efa318bcd46c2d45e5b15ac43e7da8c9dac20
Cody Constine <cody.constine@colorado.edu>
2018-01-27 09:04:27 -0700

    Fixed git ignore so Binaries are not included

--- robosub_unreal/Intermediate/Build/Win64/robosub_unreal/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:/Users/codsk_000/Documents/Unreal Projects/unreal_engine/robosub_unreal/Intermediate/Build/Win64/robosub_unreal/Development/UELinkerFixups/PCH.UELinkerFixups.h"
--- robosub_unreal/Intermediate/Build/Win64/robosub_unreal/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for D:\Users\codsk_000\Documents\Unreal Projects\unreal_engine\robosub_unreal\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
-#include "D:\Users\codsk_000\Documents\Unreal Projects\unreal_engine\robosub_unreal\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
