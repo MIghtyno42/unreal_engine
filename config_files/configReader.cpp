090ab69b2d755312b13c8b361bafce1468bc843e
MIghtyno42 <michael.min@colorado.edu>
2018-02-13 21:12:55 -0700

    Added JSON file editing/writing
    This new update to the JSON class that we're using for config allows altering JSON files by editing the JSON data structure used in this class and overwriting the old JSON files

--- config_files/configReader.cpp
+++ config_files/configReader.cpp
@@ -1,20 +1,32 @@
 #include <fstream>
 #include <iostream>
-#include <json.hpp>
+#include <C:\Users\Michael\Desktop\json.hpp>
 #include <string>
 
 using namespace std;
 
 /*
-This is the .cpp class. It is made to read the .json config file and retrieve values from it.
+This is the .cpp class. It is made to read a .json file and retrieve values from it.
+
+It can also edit external json files, add new values to keys, and add new key-value pairs altogether
 */
 
 class ConfigParser {
+
+	//this data type is from json.hpp
 	nlohmann::json jsonstore;
+
 	public:
 		ConfigParser(char* filename);
 		string get_category(char* categoryname);
+		void write_to_file(char* filename);
+		void setting_change(string setting_key_name, char* new_value);
 };
+
+
+/*
+The constructor. Each new instance of ConfigParser reads in the source from one singular external JSON file, stores the file contents, and closes the file.  
+*/
 
 ConfigParser::ConfigParser(char* filename) {
 	ifstream configFile(filename);
@@ -33,13 +45,46 @@ string ConfigParser::get_category(char* categoryname) {
 
 
 /*
-int main() {
-	ConfigParser x = ConfigParser("example.json");
 
-	
-	ofstream log;
-	log.open("log.txt");
-	log << x.get_category("address");
-	log.close();
+This function can either:
+
+*If the key name in the first parameter already exists in the JSON) Change the value of an existing key in the JSON
+
+*If the key name does not exist in the JSON) Make a new key-value pair in the JSON 
+
+*/
+
+void ConfigParser::setting_change(string setting_key_name, char* new_value) {
+	jsonstore[setting_key_name] = new_value;
+}
+
+
+
+/*
+This write function is made to rewrite the JSON setting files, for both configs and pub/sub.
+
+Use this function after you have used the function ... to update the external the JSON file.
+
+MAKE SURE THE FILENAME FOR THE JSON FILE IS WHAT YOU WANT IT TO BE (the original JSON settings file name).
+
+I suggest using the same file name as the original input JSON file's name if it is necessary to change multiple files
+*/
+
+void ConfigParser::write_to_file(char* filename) {
+	ofstream external_file(filename);
+	external_file << setw(4) << jsonstore << endl;
+	external_file.close();
+}
+
+
+/*
+This is an example of how the files and class may work.
+*/
+
+/*
+int main() {
+	ConfigParser x = ConfigParser("unrealConfig.json");
+	x.setting_change("pi", "three point one four");
+	x.write_to_file("external_test_file.json");
 }
 */
